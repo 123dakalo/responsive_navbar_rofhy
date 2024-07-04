f9755708d1ca90049886b123295c844ccefcfc67
123dakalo <tshivhasedakalo7@gmail.com>
2024-07-04 11:46:38 +0200

    first commit

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("active")
   nav.classList.toggle("active")
 });
+
+document.addEventListener("scroll", () =>{
+  const header = document.querySelector("header");
+
+  if(window.scrollY > 0){
+    header.classList.add("scrolled");
+  }else{
+    header.classList.remove("scrolled");
+  }
+});
